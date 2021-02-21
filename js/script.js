24b12c30b85814504182be7144334910ec1379f6
Denis <wolf-den@tut.by>
2021-02-21 17:42:41 +0300

    feat: slider

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.latest_slider').slick({
+      arrows: true,
+      slidesToShow: 3,
+      infinite: true,
+      slidesToScroll: 1,
+      speed: 650,
+      adaptiveHeight: true,
+   });
+});
